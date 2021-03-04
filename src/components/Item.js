50ab6d73300e1d59d34f72b9937bd9d86f47e6d5
LaraReAl <lararefojos@gmail.com>
2021-03-04 17:10:41 +0100

    feat(*): add item component
    Use new item and header in list component

---
+++ src/components/Item.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Item = ({ data }) => (
+    <div className="grid-item">
+        <img className="preview-image" src={data.thumbnail} alt={data.title}></img>
+        <div className="preview-title">{data.title}</div>
+    </div>
+)
+
+
+Item.propTypes = {
+    data: PropTypes.object.isRequired
+}
+
+export default Item;

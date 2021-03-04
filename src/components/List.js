2dbd075848aea2d5e51051ee65e94c1490c0077b
LaraReAl <lararefojos@gmail.com>
2021-03-04 11:57:13 +0100

    feat(List): add list of videos
    Use react fragments

--- src/components/List.js
+++ src/components/List.js
@@ -1,26 +1,47 @@
-import {Component} from 'react';
+import React, {Component} from 'react';
 import Loading from './Loading';
 
 export default class List extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isLoading: false
+            isLoading: false,
+            videos: null
         }
     }
 
     componentDidMount() {
         this.setState({isLoading: true});
+        // Emulate a call to external API
         setTimeout(() => {
-            this.setState({isLoading: false});
+            this.setState({
+                isLoading: false,
+                videos: [
+                    {id: 1},
+                    {id: 2},
+                    {id: 3}
+                ]
+            });
         }, 2000);
     }
 
     render() {
-        const { isLoading } = this.state;
+        const { videos, isLoading } = this.state;
         if (isLoading) {
             return (<Loading message="Loading..."/>);
         }
-        return (<div> Content </div>)
+        return (
+            <React.Fragment>
+                <div className="container">
+                    <div className="grid-container">
+                        {
+                            videos && videos.map((video, i) => {
+                                return (<span key={i}>#{video.id}</span>)
+                            })
+                        }
+                    </div>
+                </div>
+            </React.Fragment>
+        )
     }
 }

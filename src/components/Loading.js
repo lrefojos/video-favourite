f9708ff851a1935d08d7f00e9a386042d8dcd533
LaraReAl <lararefojos@gmail.com>
2021-03-04 11:26:07 +0100

    refactor(List): Use props

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,12 +1,16 @@
 import  {Component} from 'react';
+import PropTypes from 'prop-types';
 
 export default class Loading extends Component {
-
     render() {
         return (
-            <div>
-                Loading ...
+            <div className="loader">
+               {this.props.message || 'Loading ...'}
             </div>
         )
     }
+}
+
+Loading.propTypes = {
+    message: PropTypes.string.isRequired
 }

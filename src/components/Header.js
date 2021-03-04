05c290abfd73dd593db80bd14f0f5368ed8b8cdd
LaraReAl <lararefojos@gmail.com>
2021-03-04 12:19:58 +0100

    feat(*): add header component

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+ import React from 'react';
+ 
+ export const Header = () => {
+    <div className="header-content">
+        <div className="header-title-text">
+            My favourite videos
+        </div>
+        <input className="header-button-add" type="button" value="Add video"></input>
+    </div>
+}

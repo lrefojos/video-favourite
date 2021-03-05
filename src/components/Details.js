271968b2853aff3c56c4a9d7380f2a55085dc25a
LaraReAl <lararefojos@gmail.com>
2021-03-05 13:31:37 +0100

    feat(Details): add video component. Get video by id

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,17 +1,53 @@
-import React from "react";
-import { withRouter } from "react-router";
+import React, { Component } from 'react';
+import { withRouter } from 'react-router';
+import { getVideoDetail, getVideos } from '../api';
+import Loading from './Loading';
+import Video from './Video';
 
-export default class Details extends React.Component {
+export default class Details extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            isLoading: false,
+            video: null,
+            error: null
+        }
+    }
+
+    async componentDidMount() {
+        this.setState({ isLoading: true });
+        try {
+            const video = await getVideoDetail({ idVideo: this.props.match.params.id });
+            this.setState({
+                video,
+                isLoading: false
+            });
+        } catch (error) {
+            this.setState({ isLoading: false, error });
+        }
     }
 
     render() {
-        return(
+        const { isLoading, video, error } = this.state;
+        if (isLoading || !video) {
+            return (<Loading message={`Loading video (#${this.props.match.params.id})....`} />)
+        }
+        if (error) {
+            return <p className="error">{error.message}</p>
+        }
+
+        return (
             <React.Fragment>
-                HELLO {this.props.match.params.id}
+                <div className="detail-container">
+                    <Video title={video.title} embed={video.embed} />
+                </div>
+                <div className="detail-summary">
+                    <h2 className="detail-title">{video.title}</h2>
+                    <p>{video.description}</p>
+                </div>
             </React.Fragment>
         )
+
     }
 }
 

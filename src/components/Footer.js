1ad1a272ca5b01fed37acd8d1c07e740585deb0c
LaraReAl <lararefojos@gmail.com>
2021-03-06 13:15:08 +0100

    feat(*): add about component and link it

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => (
     <div className="footer-content">
-        <p>Created by: L.R</p>
+        <p>Created by: L.R   
+        <Link className="footer-link" to={'/about'}> About us</Link>
+        </p>
     </div>
+
 )
 
 export default Footer;

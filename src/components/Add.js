487b7eb454f8f4c50de6b6e46f53e89acfb8b5b4
LaraReAl <lararefojos@gmail.com>
2021-03-09 18:22:41 +0100

    feat(*): component to add new videos

---
+++ src/components/Add.js
@@ -0,0 +1,28 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Add extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: this.props.onClose,
+        }
+    }
+
+    render() {
+        const { onClose } = this.props;
+        return(
+            <div className="modal">
+                <div className="modal-content">
+                   <span className="close" onClick={onClose(false)}></span>
+                   <h2>Create a new video</h2>
+                   HERE comes the modal 
+                </div>
+            </div>
+        );
+    }
+}
+
+Add.propTypes = {
+    onClose: PropTypes.func.isRequired
+};
